f4d04f4c2d0b581494b86cb41e22440dd596ab84
Abdullah al masud <abdullahalmasud01737906772@gmail.com>
2025-01-03 21:35:14 +0600

    create login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
 // import { RouterOutlet } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
+import { RegisterComponent } from './features/auth/components/register/register.component';
 
 @Component({
   selector: 'app-root',
-  imports: [FontAwesomeModule, MatIconModule, CommonModule],
+  imports: [FontAwesomeModule, MatIconModule, CommonModule, RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'secretline_client';
+
+  isUserAvailable: boolean = false;
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      const token = localStorage.getItem('accessToken');
+      this.isUserAvailable = !!token;
+    }
+  }
+
+  // Child থেকে এই মেথড কল হবে
+  updateUserStatus(status: boolean) {
+    this.isUserAvailable = status;
+  }
 
   selectedMenu: string = 'home'; // Default selected menu
   detailsView: boolean = false; // To toggle the details view
