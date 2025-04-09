db71b51e88e66e1f9288459716745fadfa69bbd0
Abdullah al masud <abdullahalmasud01737906772@gmail.com>
2025-04-09 23:13:50 +0600

    update ci/cd pipeline

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import IncomingAudioCall from "./components/chattingDetails/audioCall/IncomingAudioCall";
 import "./index.css";
 import Home from "./pages/home/Home";
-import Login from "./pages/logins/Login";
+import Login from "./pages/login/Login";
 import { selectCurrentUser } from "./redux/features/auth/authSlice";
 import { useAppSelector } from "./redux/hooks";
 import socket from "./utils/Socket";
