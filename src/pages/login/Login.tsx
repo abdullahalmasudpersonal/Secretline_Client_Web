39a9a4b9798bf543d0456f5c4db22bc88ba1f9b4
Abdullah Al Masud <abdullahalmasud772@gmail.com>
2025-10-07 23:50:01 +0600

    login update

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -7,6 +7,7 @@ import { SubmitHandler, useForm } from "react-hook-form";
 import { setUser, TUser } from "../../redux/features/auth/authSlice";
 import { verifyToken } from "../../utils/verifyToken";
 import DemoCriential from "./DemoCriential";
+import { Box } from "@mui/material";
 
 type FormValues = {
   email: string;
@@ -54,88 +55,89 @@ const Login: React.FC = () => {
   }
 
   return (
-    <div className="loginbody">
-      <div
-        className={`container ${
-          isRightPanelActive ? "right-panel-active" : ""
-        }`}
-      >
-        <div className="form-container sign-up-container">
-          <form action="#">
-            <h1>Create Account</h1>
-            <div className="social-container">
-              <a href="#" className="social">
-                <i className="fab fa-facebook-f"></i>
-              </a>
-              <a href="#" className="social">
-                <i className="fab fa-google-plus-g"></i>
-              </a>
-              <a href="#" className="social">
-                <i className="fab fa-linkedin-in"></i>
-              </a>
-            </div>
-            <span>or use your email for registration</span>
-            <input type="text" placeholder="Name" />
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <button type="button">Sign Up</button>
-          </form>
-        </div>
+    <Box>
+      <div className="loginbody">
+        <div
+          className={`container ${isRightPanelActive ? "right-panel-active" : ""
+            }`}
+        >
+          <div className="form-container sign-up-container">
+            <form action="#">
+              <h1>Create Account</h1>
+              <div className="social-container">
+                <a href="#" className="social">
+                  <i className="fab fa-facebook-f"></i>
+                </a>
+                <a href="#" className="social">
+                  <i className="fab fa-google-plus-g"></i>
+                </a>
+                <a href="#" className="social">
+                  <i className="fab fa-linkedin-in"></i>
+                </a>
+              </div>
+              <span>or use your email for registration</span>
+              <input type="text" placeholder="Name" />
+              <input type="email" placeholder="Email" />
+              <input type="password" placeholder="Password" />
+              <button type="button">Sign Up</button>
+            </form>
+          </div>
 
-        <div className="form-container sign-in-container">
-          <form onSubmit={handleSubmit(onFinish)}>
-            <h1>Sign In</h1>
-            <div className="social-container">
-              <a href="#" className="social">
-                <i className="fab fa-facebook-f"></i>
-              </a>
-              <a href="#" className="social">
-                <i className="fab fa-google-plus-g"></i>
-              </a>
-              <a href="#" className="social">
-                <i className="fab fa-linkedin-in"></i>
-              </a>
-            </div>
-            <span>or use your account</span>
-            <input
-              type="email"
-              placeholder="Email"
-              {...register("email", { required: true })}
-            />
-            <input
-              type={passVisible ? "text" : "password"}
-              placeholder="Password"
-              {...register("password", { required: true })}
-            />
-            <p style={{ margin: "0", color: "red" }}> {errorElement}</p>
-            <a href="#">Forgot your password?</a>
-            <button type="submit">Sign In</button>
-            <DemoCriential />
-          </form>
-        </div>
+          <div className="form-container sign-in-container">
+            <form onSubmit={handleSubmit(onFinish)}>
+              <h1>Sign In</h1>
+              <div className="social-container">
+                <a href="#" className="social">
+                  <i className="fab fa-facebook-f"></i>
+                </a>
+                <a href="#" className="social">
+                  <i className="fab fa-google-plus-g"></i>
+                </a>
+                <a href="#" className="social">
+                  <i className="fab fa-linkedin-in"></i>
+                </a>
+              </div>
+              <span>or use your account</span>
+              <input
+                type="email"
+                placeholder="Email"
+                {...register("email", { required: true })}
+              />
+              <input
+                type={passVisible ? "text" : "password"}
+                placeholder="Password"
+                {...register("password", { required: true })}
+              />
+              <p style={{ margin: "0", color: "red" }}> {errorElement}</p>
+              <a href="#">Forgot your password?</a>
+              <button type="submit">Sign In</button>
+              <DemoCriential />
+            </form>
+          </div>
 
-        <div className="overlay-container">
-          <div className="overlay">
-            <div className="overlay-panel overlay-left">
-              <h1>Welcome Back!</h1>
-              <p>
-                To keep connected with us please login with your personal info
-              </p>
-              <button className="ghost" onClick={handleSignIn}>
-                Sign In
-              </button>
-            </div>
-            <div className="overlay-panel overlay-right">
-              <h1>Hello, Friend!</h1>
-              <p>Enter your personal details and start journey with us</p>
-              <button className="ghost" onClick={handleSignUp}>
-                Sign Up
-              </button>
+          <div className="overlay-container">
+            <div className="overlay">
+              <div className="overlay-panel overlay-left">
+                <h1>Welcome Back!</h1>
+                <p>
+                  To keep connected with us please login with your personal info
+                </p>
+                <button className="ghost" onClick={handleSignIn}>
+                  Sign In
+                </button>
+              </div>
+              <div className="overlay-panel overlay-right">
+                <h1>Hello, Friend!</h1>
+                <p>Enter your personal details and start journey with us</p>
+                <button className="ghost" onClick={handleSignUp}>
+                  Sign Up
+                </button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </Box>
   );
 };
 
